2d06ed65d1fdbc62a50dac128724ae1a56e1f136
frankt117 <frankjtorresjr@gmail.com>
2013-10-26 13:18:06 -0500

    Major Changes. Disabled zend layouts. Upgraded to Bootstrap 3.

---
+++ application/controllers/ExampleController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AEGIS-117
+ * Date: 10/26/13
+ * Time: 12:11 PM
+ */
+
+class ExampleController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+
+    }
+
+
+}
