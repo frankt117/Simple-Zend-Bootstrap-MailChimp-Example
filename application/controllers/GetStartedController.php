518ab316100a3ffde331b9b55d18ebb105605a87
Dustin Moorman <dustin.moorman@gmail.com>
2013-12-04 21:19:21 -0600

    adding needle headers

--- application/controllers/GetStartedController.php
+++ application/controllers/GetStartedController.php
@@ -2,10 +2,16 @@
 
 class GetStartedController extends Zend_Controller_Action
 {
+    protected $_mail_headers = '';
 
     public function init()
     {
-        /* Initialize action controller here */
+
+        $headers = "From: sales@quickcmsthemes.com\r\n";
+        $headers .= "Reply-To: sales@quickcmsthemes.com\r\n";
+        $headers .= "Return-Path: sales@quickcmsthemes.com\r\n";
+
+        $this->_mail_headers = $headers;
     }
 
     public function indexAction()
@@ -16,7 +22,7 @@ class GetStartedController extends Zend_Controller_Action
     public function thanksAction()
     {
         if($this->_request->getParams()){
-            if(false === mail('dustinmoorman@gmail.com',"From QCMST", "This is a test from QCMST", "From: sales@quickcmsthemes.com \r\n")){
+            if(false === mail('dustinmoorman@gmail.com',"From QCMST", "This is a test from QCMST", $this->_mail_headers)){
              die('MAIL SEND FAIL');
             }
         }
