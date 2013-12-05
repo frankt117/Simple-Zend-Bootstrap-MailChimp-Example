5dd6979a6e7b4e3125773e126e5eb913b9deaedf
Dustin Moorman <dustin.moorman@gmail.com>
2013-12-04 21:32:57 -0600

    making it badass

--- application/controllers/GetStartedController.php
+++ application/controllers/GetStartedController.php
@@ -4,14 +4,14 @@ class GetStartedController extends Zend_Controller_Action
 {
     protected $_mail_headers = '';
 
+    protected $_mail_to = '';
+
+    protected $_mail_message = '';
+
     public function init()
     {
-
-        $headers = "From: sales@quickcmsthemes.com\r\n";
-        $headers .= "Reply-To: sales@quickcmsthemes.com\r\n";
-        $headers .= "Return-Path: sales@quickcmsthemes.com\r\n";
-
-        $this->_mail_headers = $headers;
+        $this->defineMailTo();
+        $this->defineMailHeaders();
     }
 
     public function indexAction()
@@ -21,11 +21,39 @@ class GetStartedController extends Zend_Controller_Action
 
     public function thanksAction()
     {
-        if($this->_request->getParams()){
-            if(false === mail('dustinmoorman@gmail.com',"From QCMST", "This is a test from QCMST", $this->_mail_headers)){
-             die('MAIL SEND FAIL');
+        $post_data = $this->_request->getParams();
+
+        $this->setMailMessage($post_data);
+
+        if($post_data){
+            if(false === mail(
+                    $this->_mail_to,
+                    "New QCMST Inquiry from " . $post_data['email'],
+                    $this->_mail_message,
+                    $this->_mail_headers
+            )){
+
+                error_log('MAIL SEND FAIL! Here was the contents: ' . $this->_mail_message);
             }
         }
+    }
+
+    private function defineMailTo(){
+        $this->_mail_to = 'Dustin Moorman <dustinmoorman@gmail.com>, Frank Torres <frankjtorresjr@gmail.com>';
+    }
+
+    private function defineMailHeaders(){
+        $headers = "From: sales@quickcmsthemes.com\r\n";
+        $headers .= "Reply-To: sales@quickcmsthemes.com\r\n";
+        $headers .= "Return-Path: sales@quickcmsthemes.com\r\n";
+
+        $this->_mail_headers = $headers;
+    }
+
+    protected function setMailMessage($post_data){
+        $this->_mail_message = 'At ' . date(DATE_RSS) . ' ' . $post_data['email'] . " made a new QCMST request!\r\n";
+        $this->_mail_message .= $post_data['description'];
+
     }
 
 
